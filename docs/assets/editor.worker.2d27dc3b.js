5e3bb905660d8fc90fb4c33b44cb1a4408354c94
YongGit <1013588891@qq.com>
2022-11-16 18:18:52 +0800

    feat: docs

--- docs/assets/editor.worker.2d27dc3b.js
+++ docs/assets/editor.worker.2d27dc3b.js
@@ -1,1 +1,1 @@
-function e(){return new Worker("/assets/editor.worker.ffac8b42.js",{type:"module"})}export default e;
+function e(){return new Worker("/vue-hooks-plus-playground/docs/assets/editor.worker.ffac8b42.js",{type:"module"})}export default e;
--- docs/assets/html.worker.d881cef4.js
+++ docs/assets/html.worker.d881cef4.js
@@ -1,1 +1,1 @@
-function e(){return new Worker("/assets/html.worker.5872b30e.js",{type:"module"})}export default e;
+function e(){return new Worker("/vue-hooks-plus-playground/docs/assets/html.worker.5872b30e.js",{type:"module"})}export default e;
