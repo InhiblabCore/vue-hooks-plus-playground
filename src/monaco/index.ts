b2825427abc17f786625ed97a73169aba70f133a
YongGit <1013588891@qq.com>
2023-02-15 13:17:13 +0800

    chore: ci

--- src/monaco/index.ts
+++ src/monaco/index.ts
@@ -24,14 +24,119 @@ const setup = createSingletonPromise(async () => {
 
 	monaco.languages.typescript.javascriptDefaults.addExtraLib(
 		`
-    declare module 'vue' { ${vueTypes} }
+    declare module 'vue' {  ${vueTypes}  }
   `,
 		'ts:vue'
 	)
 
+	vhpTypes.replace(
+		`import { ComputedRef } from 'vue';
+import Cookies from 'js-cookie';
+import { createApp } from 'vue';
+import type { DebouncedFunc } from 'lodash';
+import { DeepReadonly } from 'vue';
+
+import { UnwrapNestedRefs } from 'vue';
+import { UnwrapRef } from 'vue';
+import { VueElement } from 'vue';
+import { WatchSource } from 'vue';`,
+		''
+	)
+
+	const replaceType = ` 
+  export declare class VueElement extends BaseClass {
+    private _def;
+    private _props;
+    /* Excluded from this release type: _instance */
+    private _connected;
+    private _resolved;
+    private _numberProps;
+    private _styles?;
+    constructor(_def: InnerComponentDef, _props?: Record<string, any>, hydrate?: RootHydrateFunction);
+    connectedCallback(): void;
+    disconnectedCallback(): void;
+    /**
+     * resolve inner component definition (handle possible async component)
+     */
+    private _resolveDef;
+    private _resolveProps;
+    protected _setAttr(key: string): void;
+    /* Excluded from this release type: _getProp */
+    /* Excluded from this release type: _setProp */
+    private _update;
+    private _createVNode;
+    private _applyStyles;
+}
+
+declare type BaseTypes = string | number | boolean;
+
+declare type Builtin = Primitive | Function | Date | Error | RegExp;
+
+declare type CollectionTypes = IterableCollections | WeakCollections;
+  declare const ShallowReactiveMarker: unique symbol;
+  export declare type UnwrapNestedRefs<T> = T extends Ref ? T : UnwrapRefSimple<T>;
+
+export declare type UnwrapRef<T> = T extends ShallowRef<infer V> ? V : T extends Ref<infer V> ? UnwrapRefSimple<V> : UnwrapRefSimple<T>;
+
+declare type UnwrapRefSimple<T> = T extends Function | CollectionTypes | BaseTypes | Ref | RefUnwrapBailTypes[keyof RefUnwrapBailTypes] | {
+    [RawSymbol]?: true;
+} ? T : T extends ReadonlyArray<any> ? {
+    [K in keyof T]: UnwrapRefSimple<T[K]>;
+} : T extends object & {
+    [ShallowReactiveMarker]?: never;
+} ? {
+    [P in keyof T]: P extends symbol ? T[P] : UnwrapRef<T[P]>;
+} : T;
+  declare interface ReadonlyMap<K, V> {
+    forEach(callbackfn: (value: V, key: K, map: ReadonlyMap<K, V>) => void, thisArg?: any): void;
+    get(key: K): V | undefined;
+    has(key: K): boolean;
+    readonly size: number;
+}
+  declare type Builtin = Primitive | Function | Date | Error | RegExp;
+  declare interface Ref<T = any> { value: T; }
+  declare type Cookies = any
+  declare type createApp = any
+  declare  interface DebouncedFunc<T extends (...args: any[]) => any> {
+        /**
+         * Call the original function, but applying the debounce rules.
+         *
+         * If the debounced function can be run immediately, this calls it and returns its return
+         * value.
+         *
+         * Otherwise, it returns the return value of the last invocation, or undefined if the debounced
+         * function was not invoked yet.
+         */
+        (...args: Parameters<T>): ReturnType<T> | undefined;
+
+        /**
+         * Throw away any pending invocation of the debounced function.
+         */
+        cancel(): void;
+
+        /**
+         * If there is a pending invocation of the debounced function, invoke it immediately and return
+         * its return value.
+         *
+         * Otherwise, return the value from the last invocation, or undefined if the debounced function
+         * was never invoked.
+         */
+        flush(): ReturnType<T> | undefined;
+    }
+    export declare type DeepReadonly<T> = T extends Builtin ? T : T extends Map<infer K, infer V> ? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>> : T extends ReadonlyMap<infer K, infer V> ? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>> : T extends WeakMap<infer K, infer V> ? WeakMap<DeepReadonly<K>, DeepReadonly<V>> : T extends Set<infer U> ? ReadonlySet<DeepReadonly<U>> : T extends ReadonlySet<infer U> ? ReadonlySet<DeepReadonly<U>> : T extends WeakSet<infer U> ? WeakSet<DeepReadonly<U>> : T extends Promise<infer U> ? Promise<DeepReadonly<U>> : T extends Ref<infer U> ? Readonly<Ref<DeepReadonly<U>>> : T extends {} ? {
+    readonly [K in keyof T]: DeepReadonly<T[K]>;
+} : Readonly<T>;
+
+export declare type WatchSource<T = any> = Ref<T> | ComputedRef<T> | (() => T);
+
+`
+
 	monaco.languages.typescript.javascriptDefaults.addExtraLib(
 		`
-    declare module 'vue-hooks-plus' { ${vhpTypes} }
+    declare module 'vue-hooks-plus' {
+      ${replaceType}
+      ${vhpTypes}
+  }
   `,
 		'ts:vue'
 	)
